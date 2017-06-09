0e639fc7d0060f613c617f5f9e90c624a07d30b1
Alex Pereira <alex.pereira@dataprev.gov.br>
2017-06-09 18:01:02 -0300

    sessão 3 aula 21

---
+++ exercicios_react/ex.3/component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+    <h1>Primeiro Componente React!</h1>
+    // ***Obs {} é o corpo do método
+    // () aqui neste exemplo é o escopo da expressão, na arrow function o return é implícito
+)
